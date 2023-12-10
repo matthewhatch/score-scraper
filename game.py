59b8554498bb80b344847ffb86da1812f4f4f15a
Matt Hatch <mhatch73@gmail.com>
2023-12-10 17:09:27 -0500

    colorized results

---
+++ game.py
@@ -0,0 +1,9 @@
+class Game:
+    def __init__(self, home_team, visiting_team, home_score, visiting_score, start_time, stage) -> None:
+        self.home_team = home_team
+        self.visiting_team = visiting_team
+        self.home_score = home_score
+        self.visiting_score = visiting_score
+        self.start_time = start_time
+        self.stage = stage
+
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-import requests
+import argparse
 import os
+import requests
 
 from bs4 import BeautifulSoup
 from termcolor import colored
 
-def main():
-    URL = 'https://sports.yahoo.com/nfl'
-    SELECTOR = 'li[data-carousel-item="true"]'
+def stage(time):
     FINAL_TEXT = ['FINAL', 'FINAL/OT', 'FINAL/SO']
     GAME_STAGE = ['1st', '2nd', '3rd', '4th']
+    
+    if time.split()[0].upper() in FINAL_TEXT:
+        return 'FINAL', 'light_green'
 
+    if time.split()[1] in GAME_STAGE:
+        return 'IN_PORGRESS', 'cyan'
+    
+    return 'NOT_STARTED','light_grey'
+
+def main(feed):
+    URL = f'https://sports.yahoo.com/{feed}'
+    SELECTOR = 'li[data-carousel-item="true"]'
+   
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, 'html.parser')
     schedule = soup.select(SELECTOR)
     for game in schedule:
-        COLOR = 'light_grey'
         if game:
             try:
                 datas = game.find_all('div')
@@ -22,14 +32,9 @@ def main():
                 network = datas[9].text
                 visitor = datas[18].span.text
                 home = datas[27].span.text
-            
-                if time.split()[0].upper() in FINAL_TEXT or time.split()[1] in GAME_STAGE:
-                    if time.split()[0].upper() in FINAL_TEXT:
-                        COLOR = 'light_green'
-                    else:
-                        COLOR = 'cyan'
+                game_stage, color = stage(time)
 
-                    # COLOR = 'light_green'
+                if game_stage == 'FINAL' or game_stage == 'IN_PORGRESS':
                     scores = datas[10].find_all('div')
                     home_score = scores[12].text
                     away_score = scores[3].text
@@ -37,10 +42,13 @@ def main():
                     home = f'{home_score} {home}'
                 
                 game_info = f'{time} {network} - {visitor} at {home}'
-                print(colored(game_info, COLOR))
+                print(colored(game_info, color))
             except IndexError as e:
                 pass
 
 if __name__ == '__main__':
     os.system('clear')
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--feed', '-f', type=str, default='nfl')
+    args = parser.parse_args()
+    main(args.feed)
